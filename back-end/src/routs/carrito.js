14e904f5e5e545cc0d172cf384e21de6696a123c
MarielaFernandez <54378155+MarielaFernandez@users.noreply.github.com>
2020-07-09 13:07:19 -0600

    Update carrito

--- back-end/src/routs/carrito.js
+++ back-end/src/routs/carrito.js
@@ -1,94 +1,88 @@
 const express = require('express');
 const router = express.Router();
-const connection = require ('../database');
+const connection = require('../database');
 
-const bodyParser =require ('body-parser');
+const bodyParser = require('body-parser');
 
-module.exports =router;
+module.exports = router;
 
 router.use(bodyParser.json());
 
 
-router.get('/', (req,res)=>
-{
+router.get('/', (req, res) => {
     res.send('hello word')
 });
 
-router.get('/listarCarrito', (req, res) =>
-{ 
+router.get('/listarCarrito', (req, res) => {
     const sql = 'SELECT * FROM carrito where estado="A"';
-    connection.query(sql, (err, results)=>{
-      if(err) throw error;
-      if(results.length>0){
-        res.json(results);
-      }else{
-        res.send('No hay nada')
-      }
-    } )
+    connection.query(sql, (err, results) => {
+        if (err) throw error;
+        if (results.length > 0) {
+            res.json(results);
+        } else {
+            res.send('No hay nada')
+        }
+    })
 });
 
-router.get('/consultarCarrito/:id', (req, res) =>
-{
- const {id}= req.params
-  const sql =`SELECT * FROM carrito where id_Transaccion=${id}`;
-  connection.query(sql, (err, results)=>{
-    if(err) throw error;
-    if(results.length>0){
-      res.json(results);
-    }else{
-      res.send('No hay nada')
+router.get('/consultarCarrito/:id', (req, res) => {
+    const { id } = req.params
+    const sql = `SELECT * FROM carrito where id_Transaccion=${id}`;
+    connection.query(sql, (err, results) => {
+        if (err) throw error;
+        if (results.length > 0) {
+            res.json(results);
+        } else {
+            res.send('No hay nada')
+        }
+    })
+});
+
+router.post('/agregarCarrito', (req, res) => {
+    const sql = 'INSERT INTO carrito SET ?';
+    const carrito = {
+        id_Cliente: req.body.id_Cliente,
+        id_Producto: req.body.id_Producto,
+        cantidad: req.body.cantidad,
+        subtotal: req.body.subtotal,
+        estado: "A"
+
     }
-  } ) 
-});
+    connection.query(sql, carrito, error => {
+        if (error) throw error;
+    })
 
-router.post('/agregarCarrito', (req, res) =>
-{ 
-  const sql = 'INSERT INTO carrito SET ?';
-  const usuario = {
-    id_Cliente: req.body.id_Cliente,
-    id_Producto : req.body.id_Producto,
-    cantidad: req.body.cantidad,
-    subtotal: req.body.subtotal, 
-    estado: "A"
-
-  }
-  connection.query(sql, carrito, error => {
-    if(error) throw error;
-  })
-
-  res.json(req.body)
+    res.json(req.body)
 
 });
 
-router.put('/modificarCarrito/:id', (req, res) =>
-{
-  
-  const{id} = req.params; 
-  const {id_Cliente, id_Producto, cantidad, subtotal} =
-  req.body;
+router.put('/modificarCarrito/:id', (req, res) => {
 
-  const sql = `UPDATE carrito SET id_Cliente= '${id_Cliente}' , id_Producto= '${id_Producto}', apellidos='${apellidos}',
+    const { id } = req.params;
+    const { id_Cliente, id_Producto, cantidad, subtotal } =
+    req.body;
+
+    const sql = `UPDATE carrito SET id_Cliente= '${id_Cliente}' , id_Producto= '${id_Producto}', apellidos='${apellidos}',
   cantidad='${cantidad}', subtotal='${subtotal}'
   where id_Transaccion=${id}`;
 
-  connection.query(sql, error => {
-    if(error) throw error;
-    res.send("carrito modificado");
-  })
+    connection.query(sql, error => {
+        if (error) throw error;
+        res.send("carrito modificado");
+    })
 });
 
-router.delete('/eliminarCarrito/:id', (req, res) =>
-{ 
-  const{id} = req.params; 
-  const {estado} =
-  req.body;
+router.delete('/eliminarCarrito/:id', (req, res) => {
+    const { id } = req.params;
+    const { estado } =
+    req.body;
 
-  const sql = `UPDATE carrito SET estado="I" where id_Transaccion=${id}`;
+    const sql = `UPDATE carrito SET estado="I" where id_Transaccion=${id}`;
 
-  connection.query(sql, error => {
-    if(error) throw error;
-    res.send("carrito eliminado");
-  })
+    connection.query(sql, error => {
+        if (error) throw error;
+        res.send("carrito eliminado");
+    })
 
 });
 
