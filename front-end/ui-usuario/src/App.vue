aeee47e7c14c50e7a20a6ece5aaa065af8ebe497
guels <54378086+guels@users.noreply.github.com>
2020-07-10 11:43:40 -0600

    Finalizado el iniciar

--- front-end/ui-usuario/src/App.vue
+++ front-end/ui-usuario/src/App.vue
@@ -3,19 +3,7 @@
     <div id="nav">
       <Navegacion />
 
-      <router-link to="/">Incio</router-link>|
-      <router-link to="/about">Acerca de</router-link>|
-      <router-link to="/getCategoria">Categoria</router-link>|
-      <router-link to="/getStock">Stock</router-link>|
-      <router-link to="/getCliente">Clientes</router-link>|
-      <router-link to="/getProducto">Productos</router-link>|
-      <router-link to="/getPedido">Pedidos</router-link>|
-      <router-link to="/getPrueba">Prueba</router-link>
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-|
-      <router-link to="/articulos">Articulos</router-link>
-|
+
     </div>
     <router-view />
   </div>
--- front-end/ui-usuario/src/views/Dashboard.vue
+++ front-end/ui-usuario/src/views/Dashboard.vue
@@ -1,10 +1,21 @@
 <template>
-    <div>
-        dashboard
+    <div id="app">
+        <div id="nav">
+        <router-link to="/">Incio</router-link>|
+        <router-link to="/about">Acerca de</router-link>|
+        <router-link to="/getCategoria">Categoria</router-link>|
+        <router-link to="/getStock">Stock</router-link>|
+        <router-link to="/getCliente">Clientes</router-link>|
+        <router-link to="/getProducto">Productos</router-link>|
+        <router-link to="/getPedido">Pedidos</router-link>|
+        <router-link to="/getPrueba">Prueba</router-link>
+        <router-link to="/">Home</router-link>|
+        <router-link to="/about">About</router-link>|
+        <router-link to="/articulos">Articulos</router-link>|
+        </div>
+        <router-view />
     </div>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
